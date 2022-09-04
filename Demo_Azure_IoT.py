24066f734113d58e81a67b710f49d547e3fbc4af
Patel Trupt Raxeshkumar <67566676+trupt-patel@users.noreply.github.com>
2022-09-04 11:03:32 +0530

    Add files via upload

---
+++ Demo_Azure_IoT.py
@@ -0,0 +1,63 @@
+import random
+import time
+
+# Using the Python Device SDK for IoT Hub:
+#   https://github.com/Azure/azure-iot-sdk-python
+# The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+from azure.iot.device import IoTHubDeviceClient, Message
+
+# The device connection string to authenticate the device with your IoT hub.
+# Using the Azure CLI:
+# az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+CONNECTION_STRING = "HostName=accident.azure-devices.net;DeviceId=mydevice;SharedAccessKey=e+kWbmkneT++UoQNz7/iJXlnHVYSPX5undbW/xX2yrk="
+
+# Define the JSON message to send to IoT Hub.
+TEMPREATURE = 20.0
+DISTANCE = 200
+HUMIDITY = 50
+MSG_TXT = '{{"tempreature": {tempreature},"distance": {distance}, "humidity": {humidity}}}'
+
+def iothub_client_init():
+    # Create an IoT Hub client
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
+
+def iothub_client_accident_data():
+
+    try:
+        client = iothub_client_init()
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+        while True:
+            # Build the message with simulated telemetry values.
+            tempreature = TEMPREATURE + (random.random() * 15)
+            distance = DISTANCE + (random.random() * 20)
+            humidity = HUMIDITY + (random.random() * 25)
+            msg_txt_formatted = MSG_TXT.format(tempreature=tempreature, distance=distance, humidity=humidity)
+            message = Message(msg_txt_formatted)
+
+            # Add a custom application property to the message.
+            # An IoT hub can filter on these properties without access to the message body.
+            #if pressure > 40:
+              #message.custom_properties["pressureAlert"] = "true"
+            #else:
+              #message.custom_properties["pressureAlert"] = "false"
+
+            # Send the message.
+            if(distance < 205):
+                print("Other vehicle so near then you need push the break");
+            elif(tempreature < 10):
+                print("Not Safe for Driving for any one");
+            else:
+                print( "Sending message: {}".format(message) )
+                client.send_message(message)
+                print ( "Message successfully sent" )
+                time.sleep(3)
+
+    except KeyboardInterrupt:
+        print ( "IoTHubClient sample stopped" )
+
+if __name__ == '__main__':
+    print ( "IoT Hub Quickstart #1 - Simulated device" )
+    print ( "Press Ctrl-C to exit" )
+    iothub_client_accident_data();
+Footer
